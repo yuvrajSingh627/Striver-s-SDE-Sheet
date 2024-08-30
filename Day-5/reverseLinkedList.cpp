f725d59a28f09ab74b3a5477037865336b6a7421
Yuvraj Singh <yuvsingh.17503@gmail.com>
2024-08-30 15:07:07 +0530

    Add files via upload

---
+++ Day-5/reverseLinkedList.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+
+        while(temp!=NULL){
+            ListNode* nextn = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = nextn;
+        }
+        return prev;
+    }
+};
